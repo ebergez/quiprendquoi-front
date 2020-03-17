1a37006430382ab64a9a66bef4aabf9e6cf80481
unknown <emile.bergez@hetic.net>
2020-03-17 11:07:52 +0100

    19 - Processus parallèles

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', function(req, res) {
-  res.render('index', { title: 'Qui prend quoi ?' });
+app.get("/", function(req, res) {
+  res.render("index", { title: "Qui prend quoi ?" });
 });
 
-app.post('/party', (req, res) => {
+app.post("/party", (req, res) => {
   axios
     .post(`${process.env.API_URL}/party`, req.body)
     .then(({ data }) => res.redirect(`/party/${data._id}`))
-    .catch((err) => res.send(err));
+    .catch(err => res.send(err));
 });
 
-app.get('/party/:id', (req, res) => {
+app.get("/party/:id", (req, res) => {
   axios
     .get(`${process.env.API_URL}/party/${req.params.id}`)
     .then(({ data }) =>
-      res.render('party', {
+      res.render("party", {
         party: data,
         title: data.name,
-        url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`,
-      }),
+        url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
+      })
     )
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`Front app listening on port ${process.env.PORT}!`),
+  console.log(`Front app listening on port ${process.env.PORT}!`)
 );
