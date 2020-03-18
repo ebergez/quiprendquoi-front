7bba908a5ddfb7b002c94bce5f1f20832c35f689
unknown <emile.bergez@hetic.net>
2020-03-18 10:24:20 +0100

    34 - Page offline

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
 addEventListener("install", () => {
-  console.log("Hello from the service worker");
+  event.waitUntil(
+    caches.open("offline").then(cache => {
+      cache.add("offline.html");
+    })
+  );
 });
 
 addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match("offline.html"))
+  );
 });
